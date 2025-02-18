f46d994fadaaf4815e34b21b042c61f27db9b2af
KaKa Barros <kauanbenitez04@gmail.com>
2025-02-18 19:36:12 +0000

    Integração do bootstrap no website

--- js/store.js
+++ js/store.js
@@ -1,26 +1,32 @@
+// Definindo as imagens e legendas para o slideshow
 let fotos = ["page.png", "banner.jpg", "banner2.jpg"];
-let legenda = ["Nike Metcon 8", "JUST DO IT",  "Nike Baseball Mercurial"];
+let legenda = ["Nike Metcon 8", "JUST DO IT", "Nike Baseball Mercurial"];
 
-
-function TrocarFoto(foto){
-	document.querySelector(".banner").src = "img/" + fotos[foto];
-	document.querySelector(".legenda").innerText = legenda[foto];
-	
+// Função para trocar a imagem e a legenda
+function TrocarFoto(fotoIndex) {
+    // Atualiza a imagem no carrossel
+    document.querySelector(".banner").src = "img/" + fotos[fotoIndex];
+    // Atualiza a legenda do carrossel
+    document.querySelector(".legenda").innerText = legenda[fotoIndex];
 }
 
+// Variável para controlar a foto atual
 let fotoAtual = 0;
+
+// Chama a função para trocar a foto assim que a página carrega
 TrocarFoto(fotoAtual);
 
+// Intervalo para a troca automática de fotos
+let timer = setInterval(function () {
+    fotoAtual++; // Avança para a próxima foto
+    if (fotoAtual >= fotos.length) {
+        fotoAtual = 0; // Se chegou ao final do array, volta para a primeira foto
+    }
+    // Troca a foto e a legenda
+    TrocarFoto(fotoAtual);
+}, 4000); // 4000ms = 4 segundos
 
-var timer = setInterval(function(){
-	fotoAtual++;
-	if(fotoAtual > 2){
-		fotoAtual = 0;
-	}
-	
-	TrocarFoto(fotoAtual);
-} , 4000);
-
+// Função para parar a troca automática de fotos
 function Parar() {
-	clearInterval(timer);
-  }
+    clearInterval(timer); // Limpa o intervalo e para a troca automática
+}
